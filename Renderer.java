3053d084c342d7fe21ab1c21435ccc259cf1410d
UriB1 <106737885+UriB1@users.noreply.github.com>
2024-02-29 17:07:48 +0200

    Update Renderer.java

--- Renderer.java
+++ Renderer.java
@@ -3,14 +3,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.Serial;
 
+// The Renderer class extends JPanel and is responsible for rendering the game graphics.
 class Renderer extends JPanel {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    // Overrides the paintComponent method of JPanel to customize the rendering behavior.
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (FlappyBird.flappyBird != null) {
+            // Call the repaint method of the FlappyBird instance, passing the Graphics object
             FlappyBird.flappyBird.repaint(g);
         }
     }
